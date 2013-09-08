adb36bfd428f3fb89a65be1baac93810130ccb9e
David Barber <tennesseedaved@gmail.com>
2013-09-07 22:06:05 -0500

    Initial Commit

---
+++ Tree Cutter Game/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  Tree Cutter Game
+//
+//  Created by David Barber on 9/7/13.
+//  Copyright David Barber 2013. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+int main(int argc, char *argv[]) {
+    
+    NSAutoreleasePool * pool = [[NSAutoreleasePool alloc] init];
+    int retVal = UIApplicationMain(argc, argv, nil, @"AppController");
+    [pool release];
+    return retVal;
+}
